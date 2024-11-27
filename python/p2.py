9b9adbf452bfd841637d3b1ba0614d8c22bf8d81
Sushilone <sushilonecp@gmail.com>
2024-11-27 12:40:36 +0530

    Added python p2

--- python/p2.py
+++ python/p2.py
@@ -1,28 +1,25 @@
-# Q. Display design pattern in python
+# Q. Display design pattern in python.
 """
-*
-**
-***
-****
-*****
+1
+22
+333
+4444
+55555
 
 """
-
 # Input:
 # enter a number: 5
 
-
 # Output:
-# *
-# **
-# ***
-# ****
-# *****
+# 1
+# 22
+# 333
+# 4444
+# 55555
 
 # Program code
 n = int(input("enter a number: "))
-
 for i in range(n):
     for j in range(i+1):
-        print("*", end="")
+        print(i+1,end="")
     print()
